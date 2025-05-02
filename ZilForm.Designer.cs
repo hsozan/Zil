5abdefb8ad215ebda711cb732ee2f76487761f2e
Bilgislem <Bilgislem@DESKTOP-2467RMR>
2025-05-02 17:11:46 +0300

    Proje dosyası ekle.

---
+++ ZilForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Zil
+{
+    partial class ZilForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.profileComboBox = new System.Windows.Forms.ComboBox();
+            this.newProfileButton = new System.Windows.Forms.Button();
+            this.deleteProfileButton = new System.Windows.Forms.Button();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.scheduleList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // profileComboBox
+            // 
+            this.profileComboBox.FormattingEnabled = true;
+            this.profileComboBox.Location = new System.Drawing.Point(20, 20);
+            this.profileComboBox.Name = "profileComboBox";
+            this.profileComboBox.Size = new System.Drawing.Size(150, 21);
+            this.profileComboBox.TabIndex = 0;
+            this.profileComboBox.SelectedIndexChanged += new System.EventHandler(this.profileComboBox_SelectedIndexChanged);
+            // 
+            // newProfileButton
+            // 
+            this.newProfileButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.newProfileButton.Location = new System.Drawing.Point(180, 20);
+            this.newProfileButton.Name = "newProfileButton";
+            this.newProfileButton.Size = new System.Drawing.Size(100, 23);
+            this.newProfileButton.TabIndex = 1;
+            this.newProfileButton.Text = "Yeni Program";
+            this.newProfileButton.UseVisualStyleBackColor = false;
+            this.newProfileButton.Click += new System.EventHandler(this.newProfileButton_Click);
+            // 
+            // deleteProfileButton
+            // 
+            this.deleteProfileButton.BackColor = System.Drawing.Color.IndianRed;
+            this.deleteProfileButton.Location = new System.Drawing.Point(290, 20);
+            this.deleteProfileButton.Name = "deleteProfileButton";
+            this.deleteProfileButton.Size = new System.Drawing.Size(100, 23);
+            this.deleteProfileButton.TabIndex = 2;
+            this.deleteProfileButton.Text = "Programı Sil";
+            this.deleteProfileButton.UseVisualStyleBackColor = false;
+            this.deleteProfileButton.Click += new System.EventHandler(this.deleteProfileButton_Click);
+            // 
+            // timePicker
+            // 
+            this.timePicker.CustomFormat = "HH:mm";
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePicker.Location = new System.Drawing.Point(20, 60);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.ShowUpDown = true;
+            this.timePicker.Size = new System.Drawing.Size(100, 20);
+            this.timePicker.TabIndex = 3;
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.selectFileButton.Location = new System.Drawing.Point(130, 60);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(120, 23);
+            this.selectFileButton.TabIndex = 4;
+            this.selectFileButton.Text = "Mp3 Seç";
+            this.selectFileButton.UseVisualStyleBackColor = false;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.addButton.Location = new System.Drawing.Point(260, 60);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(100, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Zamanı Ekle";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.IndianRed;
+            this.deleteButton.Location = new System.Drawing.Point(370, 60);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(100, 23);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Seçiliyi Sil";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // scheduleList
+            // 
+            this.scheduleList.FormattingEnabled = true;
+            this.scheduleList.Location = new System.Drawing.Point(20, 100);
+            this.scheduleList.Name = "scheduleList";
+            this.scheduleList.Size = new System.Drawing.Size(540, 251);
+            this.scheduleList.TabIndex = 7;
+            // 
+            // ZilForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.scheduleList);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.selectFileButton);
+            this.Controls.Add(this.timePicker);
+            this.Controls.Add(this.deleteProfileButton);
+            this.Controls.Add(this.newProfileButton);
+            this.Controls.Add(this.profileComboBox);
+            this.Name = "ZilForm";
+            this.Text = "Bahçeşehir Zil";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox profileComboBox;
+        private System.Windows.Forms.Button newProfileButton;
+        private System.Windows.Forms.Button deleteProfileButton;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.ListBox scheduleList;
+    }
+}
---
+++ ZilForm.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
+using NAudio.Wave;
+
+namespace Zil
+{
+    public partial class ZilForm : Form
+    {
+        private Dictionary<string, List<MusicSchedule>> scheduleProfiles = new Dictionary<string, List<MusicSchedule>>();
+        private string currentProfile = "Default";
+        private Timer timer = new Timer();
+        private const string profilesDirectory = "Schedules";
+
+        public ZilForm()
+        {
+            InitializeComponent();
+            Directory.CreateDirectory(profilesDirectory);
+            LoadProfiles();
+            SetupForm();
+            SetupTimer();
+        }
+
+        private void SetupForm()
+        {
+            // Initialize profile combo box
+            profileComboBox.Items.AddRange(scheduleProfiles.Keys.ToArray());
+            profileComboBox.SelectedItem = currentProfile;
+
+            // Update schedule list
+            UpdateScheduleList();
+        }
+
+        private void profileComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentProfile = profileComboBox.SelectedItem.ToString();
+            UpdateScheduleList();
+        }
+
+        private void newProfileButton_Click(object sender, EventArgs e)
+        {
+            string newProfile = $"Profile_{scheduleProfiles.Count + 1}";
+            scheduleProfiles[newProfile] = new List<MusicSchedule>();
+            profileComboBox.Items.Add(newProfile);
+            profileComboBox.SelectedItem = newProfile;
+            currentProfile = newProfile;
+            SaveProfiles();
+            UpdateScheduleList();
+        }
+
+        private void deleteProfileButton_Click(object sender, EventArgs e)
+        {
+            if (currentProfile == "Default")
+            {
+                profileComboBox.Items.Clear();
+                profileComboBox.SelectedItem = "Default";
+                currentProfile = "Default";
+                SaveProfiles();
+                UpdateScheduleList();
+                return;
+            }
+
+            if (MessageBox.Show($"Are you sure you want to delete the '{currentProfile}' profile?", "Confirm Delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // Remove profile and its JSON file
+                scheduleProfiles.Remove(currentProfile);
+                string profileFile = Path.Combine(profilesDirectory, $"{currentProfile}.json");
+                if (File.Exists(profileFile))
+                {
+                    File.Delete(profileFile);
+                }
+
+                // Switch to Default profile
+                currentProfile = "Default";
+                profileComboBox.Items.Clear();
+                profileComboBox.Items.AddRange(scheduleProfiles.Keys.ToArray());
+                profileComboBox.SelectedItem = currentProfile;
+
+                SaveProfiles();
+                UpdateScheduleList();
+            }
+        }
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectFileButton.Tag = openFileDialog.FileName;
+                    selectFileButton.Text = Path.GetFileName(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            AddSchedule(timePicker.Value, selectFileButton.Tag?.ToString());
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (scheduleList.SelectedIndex >= 0)
+            {
+                scheduleProfiles[currentProfile].RemoveAt(scheduleList.SelectedIndex);
+                SaveProfiles();
+                UpdateScheduleList();
+            }
+        }
+
+        private void SetupTimer()
+        {
+            timer.Interval = 60000; // Check every minute
+            timer.Tick += (s, e) =>
+            {
+                var now = DateTime.Now;
+                if (!scheduleProfiles.ContainsKey(currentProfile)) return;
+
+                foreach (var schedule in scheduleProfiles[currentProfile])
+                {
+                    if (now.Hour == schedule.Time.Hour && now.Minute == schedule.Time.Minute)
+                    {
+                        PlayMusic(schedule.FilePath);
+                    }
+                }
+            };
+            timer.Start();
+        }
+
+        private void AddSchedule(DateTime time, string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath) || !File.Exists(filePath))
+            {
+                MessageBox.Show("Please select a valid MP3 file.");
+                return;
+            }
+
+            // Ensure at least 20 schedules can be added (checking for daily coverage)
+
+
+            scheduleProfiles[currentProfile].Add(new MusicSchedule
+            {
+                Time = time,
+                FilePath = filePath
+            });
+
+            // Sort schedules by time
+            scheduleProfiles[currentProfile] = scheduleProfiles[currentProfile]
+                .OrderBy(s => s.Time.Hour)
+                .ThenBy(s => s.Time.Minute)
+                .ToList();
+
+            SaveProfiles();
+            UpdateScheduleList();
+        }
+
+        private void UpdateScheduleList()
+        {
+            scheduleList.Items.Clear();
+            if (scheduleProfiles.ContainsKey(currentProfile))
+            {
+                foreach (var schedule in scheduleProfiles[currentProfile])
+                {
+                    scheduleList.Items.Add($"{schedule.Time:HH:mm} - {Path.GetFileName(schedule.FilePath)}");
+                }
+            }
+        }
+
+        private void PlayMusic(string filePath)
+        {
+            try
+            {
+                using (var mp3Reader = new Mp3FileReader(filePath))
+                using (var waveOut = new WaveOutEvent())
+                {
+                    waveOut.Init(mp3Reader);
+                    waveOut.Play();
+                    // Wait for playback to complete
+                    while (waveOut.PlaybackState == PlaybackState.Playing)
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error playing MP3: {ex.Message}");
+            }
+        }
+
+        private void LoadProfiles()
+        {
+            scheduleProfiles["Default"] = new List<MusicSchedule>();
+            foreach (var file in Directory.GetFiles(profilesDirectory, "*.json"))
+            {
+                var json = File.ReadAllText(file);
+                var profileName = Path.GetFileNameWithoutExtension(file);
+                scheduleProfiles[profileName] = JsonSerializer.Deserialize<List<MusicSchedule>>(json) ?? new List<MusicSchedule>();
+            }
+        }
+
+        private void SaveProfiles()
+        {
+            foreach (var profile in scheduleProfiles)
+            {
+                var json = JsonSerializer.Serialize(profile.Value, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(Path.Combine(profilesDirectory, $"{profile.Key}.json"), json);
+            }
+        }
+    }
+
+    [Serializable]
+    public class MusicSchedule
+    {
+        public DateTime Time { get; set; }
+        public string FilePath { get; set; }
+    }
+}
